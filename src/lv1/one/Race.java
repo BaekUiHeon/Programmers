9e92176f0b53aa11083c9d6ea36356d57042b384
BaekUiHeon <135290607+BaekUiHeon@users.noreply.github.com>
2023-11-12 22:42:45 +0900

    달리기경주
    달리기경주

---
+++ src/lv1/one/Race.java
@@ -0,0 +1,13 @@
+package lv1.one;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Race {	//달리기 경주 
+    
+	public String[] solution(String[] players, String[] callings) {
+    	Map<String,Object> map = new HashMap<>(); 
+    	
+        return players;
+    }
+}
