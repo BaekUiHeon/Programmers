e2ec3fc39b099c1357eedd5c913ed6887b1e4da0
BaekUiHeon <135290607+BaekUiHeon@users.noreply.github.com>
2023-11-16 03:35:24 +0900

    시간복잡도에 따른 미통과로 해쉬함수 사용
    시간복잡도에 따른 미통과로 해쉬함수 사용

--- src/lv1/one/Race.java
+++ src/lv1/one/Race.java
@@ -6,8 +6,29 @@ import java.util.Map;
 public class Race {	//달리기 경주 
     
 	public String[] solution(String[] players, String[] callings) {
-    	Map<String,Object> map = new HashMap<>(); 
+    	Map<String,Integer> playermap = new HashMap<>();
+    	Map<Integer,String> playerRank = new HashMap<>();
     	
-        return players;
+    	for(int i=0;i<players.length;i++) {
+    		playermap.put(players[i],i);
+    		playerRank.put(i,players[i]);
+    	}
+    	
+    	int rank;
+    	String calledPlayer;
+    	String prevPlayer;
+    	for(int i=0;i<callings.length;i++) {
+    		calledPlayer=callings[i];
+    		rank=playermap.get(calledPlayer);
+    		prevPlayer=playerRank.get(rank-1);
+    		playermap.put(prevPlayer, rank);
+    		playermap.put(calledPlayer,rank-1);
+    		playerRank.put(rank,prevPlayer);
+    		playerRank.put(rank-1, calledPlayer);
+    	}
+    	for(int i=0; i<playermap.size();i++) {
+    		players[i]=playerRank.get(i);
+    	}
+    	return players;
     }
 }
